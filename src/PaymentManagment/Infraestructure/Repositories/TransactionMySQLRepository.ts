c64bf97b8d858d381777172ce19f9335fa5adaf5
CassDeLaRy <mauriciomtz893@gmail.com>
2024-06-22 15:40:05 -0600

    [ADD] New functionality added in createTransactionMethod

--- src/PaymentManagment/Infraestructure/Repositories/TransactionMySQLRepository.ts
+++ src/PaymentManagment/Infraestructure/Repositories/TransactionMySQLRepository.ts
@@ -4,7 +4,40 @@ import { TransactionModel } from "../Models/MySQL/TransactionModel";
 export class TransactionMySQLRepository implements ITransaction {
 
     async create(membershipName: string, status: string, userUUID: string, shipmentUUID: string, amount: number, date: Date): Promise<any> {
-        throw new Error("Method not implemented.");
+        try{
+            if(membershipName){
+                const data = await TransactionModel.create({
+                    membershipName,
+                    status,
+                    userUUID,
+                    amount,
+                    date
+                });
+    
+                return {
+                    status: 201,
+                    data: data.toJSON()
+                };
+            }else{
+                const data = await TransactionModel.create({
+                    status,
+                    userUUID,
+                    shipmentUUID,
+                    amount,
+                    date
+                });
+    
+                return {
+                    status: 201,
+                    data: data.toJSON()
+                };
+            }
+        }catch(error){
+            return {
+                status: 500,
+                message: 'Internal server error',
+            };
+        }
     }
 
     async getMembershipByUser(userUUID: string): Promise<any> {
